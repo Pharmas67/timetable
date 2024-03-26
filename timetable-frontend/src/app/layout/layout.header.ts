d7a08b021d6c25ecbbe42effe548b61b8827f9ae
root <root@DESKTOP-T226VTF.localdomain>
2024-03-26 18:22:57 +0100

    initial project

---
+++ timetable-frontend/src/app/layout/layout.header.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './layout.header.html',
+    styleUrls: ['./layout.header.css']
+})
+export class NameComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void { }
+}
